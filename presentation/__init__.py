96f9b36f3c456899eaa64cec3a726e2f6c0c3edb
WildCodeWolf <dustin@lischke-online.de>
2024-07-05 10:27:15 +0200

    added singular colors, set up default style

--- presentation/__init__.py
+++ presentation/__init__.py
@@ -16,6 +16,7 @@ _colors = {
     'grey7': '#EBEBEB',
     'grey8': '#EDF0F2',
 }
+
 def hex_code(color):
     """Returns the hex code for the given color.
 
@@ -32,9 +33,6 @@ def hex_code(color):
     """
     return _colors[color]
 
-sns.set_theme(style='whitegrid', context='paper')
-sns.set(rc={'figure.facecolor': hex_code('background_grey')})
-
 class Colors():
     """This class holds the various color palettes we want to use in our presenttion.
     
@@ -47,18 +45,47 @@ class Colors():
 
     selected_greys = sns.color_palette([color for color in _colors.values()][6:])
     """Discrete palette of selected grey colors."""
-    blues  = sns.dark_palette(hex_code('blue'))
-    """Discrete palette of blue colors."""
-    blues_c  = sns.dark_palette(hex_code('blue'), as_cmap=True)
-    """Continuous palette of blue colors."""
+    blue = hex_code('blue')
+    """Hex code for the selected blue color. Use it for single-color arguments in plots."""
+    blues = sns.dark_palette(hex_code('blue'))
+    """Discrete palette of blue colors, from dark to bright."""
+    blues_r = blues.reverse()
+    """Discrete palette of blue colors, from bright to dark."""
+    blues_c = sns.dark_palette(hex_code('blue'), as_cmap=True)
+    """Continuous palette of blue colors, from dark to bright."""
+    blues_cr = blues_c.reversed()
+    """Continuous palette of blue colors, from bright to dark."""
+    purple = hex_code('purple')
+    """Hex code for the selected purple color. Use it for single-color arguments in plots."""
     purples = sns.dark_palette(hex_code('purple'))
-    """Discrete palette of purple colors."""
+    """Discrete palette of purple colors, from dark to bright."""
+    purples_r = purples.reverse()
+    """Discrete palette of purple colors, from bright to dark."""
     purples_c = sns.dark_palette(hex_code('purple'), as_cmap=True)
-    """Continuous palette of purple colors."""
+    """Continuous palette of purple colors, from dark to bright."""
+    purples_cr = purples_c.reversed()
+    """Continuous palette of purple colors, from bright to dark."""
+    grey = hex_code('grey3')
+    """Hex code for the selected grey color. Use it for single-color arguments in plots."""
     greys = sns.dark_palette(hex_code('background_grey'))
     """Discrete palette of grey colors terminating in the background grey."""
+    greys_r = greys.reverse()
+    """Discrete palette of grey colors starting from the background grey."""
     greys_c = sns.dark_palette(hex_code('background_grey'), as_cmap=True)
     """Continuous palette of grey colors terminating in the background grey."""
+    greys_cr = greys_c.reversed()
+    """Continuous palette of grey colors starting from the background grey."""
+
+sns.set_theme(
+    style='whitegrid',
+    context='paper',
+    palette=Colors.blues,
+    #font='sans_serif',      # breaks the style for some reason!
+    rc={
+        'figure.facecolor': hex_code('background_grey'),
+        'axes.facecolor': hex_code('background_grey'),
+    }
+)
 
 if __name__ == '__main__':
     print("This file is not meant to be run like this!")
