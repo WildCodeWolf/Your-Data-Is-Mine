be3bb6870202916c498d5e1b077068e9b1f83a78
WildCodeWolf <dustin@lischke-online.de>
2024-07-24 15:32:07 +0200

    extended column and file support for the network traffic notebooks

--- utils/network_traffic.py
+++ utils/network_traffic.py
@@ -6,8 +6,47 @@ import pandas as pd
 class File:
     """Contains the file names for all csv files for the credit card dataset."""
 
-    #correlations = 'data/network-traffic/correlations.csv'
-    #"""The file path to the correlation coefficients for the target column."""
+    correlations_attack_identification_label = 'data/network-traffic/correlations_id_l.csv'
+    """The file path to the correlation coefficients for the attack identification column for the label encoded training data."""
+
+    correlations_attack_identification_label_scaled = 'data/network-traffic/correlations_id_ls.csv'
+    """The file path to the correlation coefficients for the attack identification column for the scaled and label encoded training data."""
+
+    correlations_attack_identification_1hot = 'data/network-traffic/correlations_id_1h.csv'
+    """The file path to the correlation coefficients for the attack identification column for the one-hot encoded training data."""
+
+    correlations_attack_identification_1hot_scaled = 'data/network-traffic/correlations_id_1hs.csv'
+    """The file path to the correlation coefficients for the attack identification column for the scaled and one-hot encoded training data."""
+
+    correlations_attack_classification_label = 'data/network-traffic/correlations_cl_l.csv'
+    """The file path to the correlation coefficients for the attack classification column for the label encoded training data."""
+
+    correlations_attack_classification_label_scaled = 'data/network-traffic/correlations_cl_ls.csv'
+    """The file path to the correlation coefficients for the attack classification column for the scaled and label encoded training data."""
+
+    correlations_attack_classification_1hot_dos = 'data/network-traffic/correlations_cl_1h_dos.csv'
+    """The file path to the correlation coefficients for the attack classification column for the one-hot encoded DOS target training data."""
+
+    correlations_attack_classification_1hot_dos_scaled = 'data/network-traffic/correlations_cl_1hs_dos.csv'
+    """The file path to the correlation coefficients for the attack classification column for the scaled and one-hot encoded DOS target training data."""
+
+    correlations_attack_classification_1hot_probe = 'data/network-traffic/correlations_cl_1h_probe.csv'
+    """The file path to the correlation coefficients for the attack classification column for the one-hot encoded Probe target training data."""
+
+    correlations_attack_classification_1hot_probe_scaled = 'data/network-traffic/correlations_cl_1hs_probe.csv'
+    """The file path to the correlation coefficients for the attack classification column for the scaled and one-hot encoded Probe target training data."""
+
+    correlations_attack_classification_1hot_r2l = 'data/network-traffic/correlations_cl_1h_r2l.csv'
+    """The file path to the correlation coefficients for the attack classification column for the one-hot encoded R2L target training data."""
+
+    correlations_attack_classification_1hot_r2l_scaled = 'data/network-traffic/correlations_cl_1hs_r2l.csv'
+    """The file path to the correlation coefficients for the attack classification column for the scaled and one-hot encoded R2L target training data."""
+
+    correlations_attack_classification_1hot_u2r = 'data/network-traffic/correlations_cl_1h_u2r.csv'
+    """The file path to the correlation coefficients for the attack classification column for the one-hot encoded U2R target training data."""
+
+    correlations_attack_classification_1hot_u2r_scaled = 'data/network-traffic/correlations_cl_1hs_u2r.csv'
+    """The file path to the correlation coefficients for the attack classification column for the scaled and one-hot encoded U2R target training data."""
 
     original_training = 'data/network-traffic/kddcup.data.csv'
     """The file path to the original network traffic dataset."""
@@ -15,29 +54,77 @@ class File:
     original_verification = 'data/network-traffic/corrected.csv'
     """The file path to the original network traffic verification dataset."""
 
-    preprocessed_training = 'data/network-traffic/preprocessed_training.csv'
-    """The file path to the preprocessed network traffic training dataset."""
+    training_attack_identification = 'data/network-traffic/training_id.csv'
+    """The file path to the attack identification training data frame."""
 
-    preprocessed_verification = 'data/network-traffic/preprocessed_verification.csv'
-    """The file path to the preprocessed network traffic verification dataset."""
+    training_attack_identification_1hot = 'data/network-traffic/training_id_1h.csv'
+    """The file path to the one-hot encoded attack identification training data frame."""
 
-    training = 'data/network-traffic/training_df.csv'
-    """The file path to the training data frame."""
+    training_attack_identification_1hot_scaled = 'data/network-traffic/training_id_1hs.csv'
+    """The file path to the scaled and one-hot encoded attack identification training data frame."""
 
-    training_scaled = 'data/network-traffic/training_df_s.csv'
-    """The file path to the scaled training data frame."""
+    training_attack_identification_label = 'data/network-traffic/training_id_l.csv'
+    """The file path to the label encoded attack identification training data frame."""
 
-    training_undersampled = 'data/network-traffic/training_df_u.csv'
-    """The file path to the undersampled training data frame."""
+    training_attack_identification_label_scaled = 'data/network-traffic/training_id_ls.csv'
+    """The file path to the scaled and label encoded attack identification training data frame."""
 
-    training_undersampled_scaled = 'data/network-traffic/training_df_us.csv'
-    """The file path to the undersampled scaled training data frame."""
+    training_attack_identification_scaled = 'data/network-traffic/training_id_s.csv'
+    """The file path to the scaled attack identification training data frame."""
 
-    verification = 'data/network-traffic/verification_df.csv'
-    """The file path to the verification data frame."""
+    training_attack_classification = 'data/network-traffic/training_cl.csv'
+    """The file path to the attack classification training data frame."""
 
-    verification_scaled = 'data/network-traffic/verification_df_s.csv'
-    """The file path to the scaled verification data frame."""
+    training_attack_classification_1hot = 'data/network-traffic/training_cl_1h.csv'
+    """The file path to the one-hot encoded attack classification training data frame."""
+
+    training_attack_classification_1hot_scaled = 'data/network-traffic/training_cl_1hs.csv'
+    """The file path to the scaled and one-hot encoded attack classification training data frame."""
+
+    training_attack_classification_label = 'data/network-traffic/training_cl_l.csv'
+    """The file path to the label encoded attack classification training data frame."""
+
+    training_attack_classification_label_scaled = 'data/network-traffic/training_cl_ls.csv'
+    """The file path to the scaled and label encoded attack classification training data frame."""
+
+    training_attack_classification_scaled = 'data/network-traffic/training_cl_s.csv'
+    """The file path to the scaled attack classification training data frame."""
+
+    verification_attack_identification = 'data/network-traffic/verification_id.csv'
+    """The file path to the attack identification verification data frame."""
+
+    verification_attack_identification_1hot = 'data/network-traffic/verification_id_1h.csv'
+    """The file path to the one-hot encoded attack identification verification data frame."""
+
+    verification_attack_identification_1hot_scaled = 'data/network-traffic/verification_id_1hs.csv'
+    """The file path to the scaled and one-hot encoded attack identification verification data frame."""
+
+    verification_attack_identification_label = 'data/network-traffic/verification_id_l.csv'
+    """The file path to the label encoded attack identification verification data frame."""
+
+    verification_attack_identification_label_scaled = 'data/network-traffic/verification_id_ls.csv'
+    """The file path to the scaled and label encoded attack identification verification data frame."""
+
+    verification_attack_identification_scaled = 'data/network-traffic/verification_id_s.csv'
+    """The file path to the scaled attack identification verification data frame."""
+
+    verification_attack_classification = 'data/network-traffic/verification_cl.csv'
+    """The file path to the attack classification verification data frame."""
+
+    verification_attack_classification_1hot = 'data/network-traffic/verification_cl_1h.csv'
+    """The file path to the one-hot encoded attack classification verification data frame."""
+
+    verification_attack_classification_1hot_scaled = 'data/network-traffic/verification_cl_1hs.csv'
+    """The file path to the scaled and one-hot encoded attack classification verification data frame."""
+
+    verification_attack_classification_label = 'data/network-traffic/verification_cl_l.csv'
+    """The file path to the label encoded attack classification verification data frame."""
+
+    verification_attack_classification_label_scaled = 'data/network-traffic/verification_cl_ls.csv'
+    """The file path to the scaled and label encoded attack classification verification data frame."""
+
+    verification_attack_classification_scaled = 'data/network-traffic/verification_cl_s.csv'
+    """The file path to the scaled attack classification verification data frame."""
 
     @staticmethod
     def load(file):
@@ -150,17 +237,43 @@ class Col:
     ]
     """The column names (in order)."""
 
+    target_type = _all_pretty[-3]
+    """The target type column."""
+    target_classification = _all_pretty[-2]
+    """The target classification column."""
+    target_identification = _all_pretty[-1]
+    """The target identification column."""
+
+    target_dos = target_classification + '_dos'
+    """The one-hot encoded DOS target column."""
+    target_probe = target_classification + '_probe'
+    """The one-hot encoded Probe target column."""
+    target_r2l = target_classification + '_r2l'
+    """The one-hot encoded R2L target column."""
+    target_u2r = target_classification + '_u2r'
+    """The one-hot encoded U2R target column."""
+
     @staticmethod
-    def top(n, correlations=True):
+    def top(n, target_identification=True, correlations=True, label_encoded=True, one_hot_target='dos'):
         """Return the top `n` features in terms of absolute correlation coefficient.
 
         Parameters
         ----------
         n : int
             Number of features to return.
+        target_identification : bool
+            If `True`, retrieve a feature ranking for the attack detection target.
+            If `False`, do so for a ranking for the attack classification target.
         correlations : bool
             If `True`, use correlation coefficients. If `False`, use data from
             feature importance analysis instead.
+        label_encoded : bool
+            If `True`, retrieve data from a ranking based on label encoded data.
+            If `False`, do so on a ranking based on one-hot encoded data.
+        one_hot_target : `{'dos', 'probe', 'r2l', 'u2r'}`, default: `'dos'`
+            If `label_encoded=False`, then this value is used to pick the correlation
+            matching the desired target column.  Has no effect for other types of
+            encodings.
         
         Returns
         -------
@@ -170,14 +283,38 @@ class Col:
 
         See Also
         --------
-        Col.above(theshold, correlations=True)
+        Col.above(threshold, target_identification=True, correlations=True, label_encoded=True)
         """
-        file = File.correlations if correlations else ""
+        match target_identification, correlations, label_encoded:
+            case False, False, False:
+                file = ''
+            case False, False, True:
+                file = ''
+            case False, True, False:
+                match one_hot_target.lower():
+                    case 'dos':
+                        file = File.correlations_attack_classification_1hot_dos
+                    case 'probe':
+                        file = File.correlations_attack_classification_1hot_probe
+                    case 'r2l':
+                        file = File.correlations_attack_classification_1hot_r2l
+                    case 'u2r':
+                        file = File.correlations_attack_classification_1hot_u2r
+            case False, True, True:
+                file = File.correlations_attack_classification_label
+            case True, False, False:
+                file = ''
+            case True, False, True:
+                file = ''
+            case True, True, False:
+                file = File.correlations_attack_identification_1hot
+            case True, True, True:
+                file = File.correlations_attack_identification_label
         ranking = pd.read_csv(file, index_col=None)
         return ranking[:n]
     
     @staticmethod
-    def above(threshold, correlations=True):
+    def above(threshold, target_identification=True, correlations=True, label_encoded=True):
         """Return all features with an absolute correlation coefficient above
         the given `threshold`.
 
@@ -185,9 +322,15 @@ class Col:
         ----------
         threshold : float
             Lower limit of the correlation coeffients to be returned.
+        target_identification : bool
+            If `True`, retrieve a feature ranking for the attack detection target.
+            If `False`, do so for a ranking for the attack classification target.
         correlations : bool
             If `True`, use correlation coefficients. If `False`, use data from
             feature importance analysis instead.
+        label_encoded : bool
+            If `True`, retrieve data from a ranking based on label encoded data.
+            If `False`, do so on a ranking based on one-hot encoded data.
         
         Returns
         -------
@@ -197,9 +340,25 @@ class Col:
 
         See Also
         --------
-        Col.top(n, correlations=True)
+        Col.top(n, target_identification=True, correlations=True, label_encoded=True)
         """
-        file = File.correlations if correlations else ""
+        match target_identification, correlations, label_encoded:
+            case False, False, False:
+                file = ''
+            case False, False, True:
+                file = ''
+            case False, True, False:
+                file = ''
+            case False, True, True:
+                file = ''
+            case True, False, False:
+                file = ''
+            case True, False, True:
+                file = ''
+            case True, True, False:
+                file = File.correlations_attack_identification_1hot
+            case True, True, True:
+                file = File.correlations_attack_identification_label
         ranking = pd.read_csv(file, index_col=None)
         return ranking[ranking['Coefficient'] >= threshold]
 
